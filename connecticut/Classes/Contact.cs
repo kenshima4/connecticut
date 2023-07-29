c54b6c75b7dc09e25645509eab2950e79032027e
kenshima4 <kenneth.shimabukuro@gmail.com>
2023-07-29 20:34:47 +0200

    created contact class

--- connecticut/Classes/Contact.cs
+++ connecticut/Classes/Contact.cs
@@ -7,7 +7,35 @@ namespace connecticut.Classes
 {
     public class Contact
     {
-        Contact() { 
+        private string name;
+        private string surname;
+        private string email;
+
+        private List<Client> linkedClients;
+        Contact(string name, string surname, string email) {
+            this.linkedClients = new List<Client>();
+            this.name = name;
+            this.surname = surname;
+            this.email = email;
+
+
+        }
+
+        public string getName() {
+            return this.name;
+        }
+
+        public string getSurname()
+        {
+            return this.surname;
+        }
+        public string getEmail()
+        {
+            return this.email;
+        }
+
+        public List<Client> getLinkedClients(){
+            return linkedClients;
         }
     }
 }
